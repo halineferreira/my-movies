ed808d613d4f1b8229e9e892decff2b72abe1da2
Haline Ferreira <halineferreira@Halines-MacBook-Pro.local>
2021-04-17 22:29:09 -0300

    Aula 12. Criando um SPA - Parte 3 - API

---
+++ src/api/MoviesService.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const API_KEY = 'e1f52d780c2e1473aaf09b259fa5de92';
+const BASE_URL = 'https://api.themoviedb.org/3/';
+
+const withBaseUrl = (path) => `${BASE_URL}${path}?api_key=${API_KEY}`;
+
+export class MoviesService {
+    static getMovies() {
+        return axios(withBaseUrl('movie/popular'));
+    }
+}
